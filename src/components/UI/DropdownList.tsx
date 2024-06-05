9fc472cc9f2a3e16963d87162442dd8d43cec27a
Phost <cucchiaraquentin@gmail.com>
2024-06-05 16:10:22 +0200

    animal register

---
+++ src/components/UI/DropdownList.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+const DropdownList = ({
+  content,
+  listContent,
+}: {
+  content: string;
+  listContent: any;
+}) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const router = useRouter();
+
+  if (!isActive) {
+    return (
+      <div>
+        <button onClick={() => setIsActive(true)}>
+          {content}
+          {' v'}
+        </button>
+      </div>
+    );
+  }
+
+  if (isActive) {
+    return (
+      <div>
+        <button onClick={() => setIsActive(false)}>
+          {content}
+          {' ^'}
+        </button>
+        <ul className="absolute">
+          {listContent.map((element: string) => {
+            return (
+              <li
+                className="flex cursor-pointer border-t-2 bg-slate-300 px-10"
+                key={element}
+                onClick={() => {
+                  router.push('/#');
+                }}
+              >
+                {element}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+};
+
+export default DropdownList;
